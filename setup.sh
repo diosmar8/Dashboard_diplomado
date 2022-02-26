5eebef71471540a8ee4f3fb2f663d317b7cb922b
diosmar8 <62900631+diosmar8@users.noreply.github.com>
2022-02-25 20:47:03 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 mkdir -p ~/.streamlit/
 echo "
 [theme]
-base="light"
-backgroundColor="#f3f3f3"
-secondaryBackgroundColor="#418c4d"
-font="monospace"
+base='light'
+backgroundColor='#f3f3f3'
+secondaryBackgroundColor='#418c4d'
+font='monospace'
 [server]
 headless = true
 enableCORS=false
